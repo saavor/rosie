1e4713d2c9233588d29f22851c930dae2d9931ec
Nova Malone <saavor@users.noreply.github.com>
2023-07-16 19:17:32 -0400

    use spaces not tabs

--- src/init.lua
+++ src/init.lua
@@ -1,7 +1,7 @@
 --[[
     rosie
     written by nova <3
-	(c) 2023
+    (c) 2023
 ]]--
 
 local rosie = {}
@@ -12,52 +12,52 @@ local RunService = game:GetService("RunService")
 rosie.systems = {} -- systems table
 
 function rosie.loadSystem(system: ModuleScript)
-	table.insert(rosie.systems, {
-		name = system.Name,
-		module = require(system)
-	}) 
+    table.insert(rosie.systems, {
+        name = system.Name,
+        module = require(system)
+    }) 
 end
 
 function rosie.loadSystems(systems)
-	for _,system in ipairs(systems) do
-		rosie.loadSystem(system)
-	end
+    for _,system in ipairs(systems) do
+        rosie.loadSystem(system)
+    end
 end
 
 function rosie.loadSystemsFromFolder(folderPath: Folder)
-	for _,system in ipairs(folderPath:GetChildren()) do 
-		if not system:IsA("ModuleScript") then continue end -- If not a modulescript, then skip
-		rosie.loadSystem(system)
-	end
+    for _,system in ipairs(folderPath:GetChildren()) do 
+        if not system:IsA("ModuleScript") then continue end -- If not a modulescript, then skip
+        rosie.loadSystem(system)
+    end
 end
 
 function rosie.start()
-	for _,system in ipairs(rosie.systems) do
-		
-		-- run system start functions
-		if system.module.start then
-			system.module:start()
-		end
-		
-		-- bind update functions
-		if system.module.update then
-			if RunService:IsServer() then
-				return error("[rosie] update can only be used on the client!")
-			end
-			
-			RunService:BindToRenderStep(system.name, Enum.RenderPriority.Camera.Value-1, function (delta: number)
-				system.module:update(delta)
-			end)
-		end
-		
-		-- bind tick functions
-		if system.module.tick then
-			RunService.Heartbeat:Connect(function (delta: number)
-				system.module:tick(delta)
-			end)
-		end
-		
-	end
+    for _,system in ipairs(rosie.systems) do
+        
+        -- run system start functions
+        if system.module.start then
+            system.module:start()
+        end
+        
+        -- bind update functions
+        if system.module.update then
+            if RunService:IsServer() then
+                return error("[rosie] update can only be used on the client!")
+            end
+            
+            RunService:BindToRenderStep(system.name, Enum.RenderPriority.Camera.Value-1, function (delta: number)
+                system.module:update(delta)
+            end)
+        end
+        
+        -- bind tick functions
+        if system.module.tick then
+            RunService.Heartbeat:Connect(function (delta: number)
+                system.module:tick(delta)
+            end)
+        end
+        
+    end
 end
 
 return rosie
